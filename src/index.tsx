935f38294a0043a4760e33b1aa13f959629904eb
陆翔 <9830080+vjxzlyx1398179752@user.noreply.gitee.com>
2024-06-30 04:07:07 +0800

    fix: 修改导出方式

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,5 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fas);
 
-import * as components from "./components";
-import * as hooks from "./hooks";
-
-export { components, hooks };
+export * from "./hooks";
+export * from "./components";
